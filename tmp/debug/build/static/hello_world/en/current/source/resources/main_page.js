14dda950d6a56cde91a95144a023a813132f16a1
Kristian Mandrup <kmandrup@gmail.com>
2009-12-18 10:04:32 +0100

    first commit

---
+++ tmp/debug/build/static/hello_world/en/current/source/resources/main_page.js
@@ -0,0 +1,30 @@
+// ==========================================================================
+// Project:   HelloWorld - mainPage
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals HelloWorld */
+
+// This page describes the main user interface for your application.  
+HelloWorld.mainPage = SC.Page.design({
+
+  // The main pane is made visible on screen as soon as your app is loaded.
+  // Add childViews to this pane for views to display immediately on page 
+  // load.
+  mainPane: SC.MainPane.design({
+    childViews: 'labelView buttonView'.w(),    
+    labelView: SC.LabelView.design({
+      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
+      textAlign: SC.ALIGN_CENTER,
+      tagName: "h1", 
+      valueBinding: "HelloWorld.appController.greeting"	
+    }),
+	buttonView: SC.ButtonView.design({      
+	  layout: { centerX: 0, centerY: 20, width: 250, height: 18 },      
+	  title: "Change Title",      
+	  action: "toggleGreeting",      
+	  target: "HelloWorld.appController"     
+	})
+  })
+
+});
+; if ((typeof SC !== 'undefined') && SC && SC.scriptDidLoad) SC.scriptDidLoad('hello_world');
